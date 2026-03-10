0abc99aab7f0d54756d9362718343154a6df6c43
Reuben Waring <reuben.waring@outlook.com>
2026-03-10 15:54:47 +0000

    Initial layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+</script>
+
+<title>Reuben Waring's Website</title>
+<div class="flex flex-col justify-center items-center h-screen ml-2.5 flex-wrap">
+	<div class="flex flex-row justify-center items-center ml-2.5 flex-wrap-reverse">
+		<div class="flex flex-col w-[50%]">
+			<h1 class="ibm-plex-serif-bold">Hello! I'm Reuben Waring</h1>
+			<p class="ibm-plex-mono-regular">I'm a second year Computer Science student at Sheffield Hallam University and keen programmer. On here, you'll find a varied selection of my projects from both University and personal work.</p>
+		</div>
+		<img src="/IMG_2946.JPG" alt="Reuben Waring Headshot" class="rounded-full scale-50"/>
+	</div>
+</div>
