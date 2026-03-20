b69ba0df62b4d75a327291c95b2fa20494229258
Reuben Waring <reuben.waring@outlook.com>
2026-03-20 23:04:06 +0000

    Add Meta Tags

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,5 +5,29 @@
 	let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head>
+	<link rel="icon" href={favicon} />
+	<!-- HTML Meta Tags -->
+	<meta name="description" content="Sheffield-based Computer Science student looking for a placement/internship opportunity" />
+
+	<!-- Google / Search Engine Tags -->
+	<meta itemprop="name" content="Reuben Waring&#039;s Website" />
+	<meta itemprop="description" content="Sheffield-based Computer Science student looking for a placement/internship opportunity" />
+	<meta itemprop="image" content="https://reubwa.github.io/IMG_2946.JPG" />
+
+	<!-- Facebook Meta Tags -->
+	<meta property="og:url" content="https://reubwa.github.io/" />
+	<meta property="og:type" content="website" />
+	<meta property="og:title" content="Reuben Waring&#039;s Website" />
+	<meta property="og:description" content="Sheffield-based Computer Science student looking for a placement/internship opportunity" />
+	<meta property="og:image" content="https://reubwa.github.io/IMG_2946.JPG" />
+
+	<!-- Twitter Meta Tags -->
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta name="twitter:title" content="Reuben Waring&#039;s Website" />
+	<meta name="twitter:description" content="Sheffield-based Computer Science student looking for a placement/internship opportunity" />
+	<meta name="twitter:image" content="https://reubwa.github.io/IMG_2946.JPG" />
+
+	<!-- Meta Tags Generated via https://heymeta.com -->
+</svelte:head>
 {@render children()}
